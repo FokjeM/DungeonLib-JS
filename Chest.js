4cbb13ec5537c8168d6d66ee821d4383ecf1318b
Riven Skaye <pretender0906@gmail.com>
2019-10-23 18:06:20 +0200

    Some progress, not much

--- Chest.js
+++ Chest.js
@@ -1,3 +1,10 @@
 class Chest {
-    
+    constructor(equipment) {
+        if(!(equipment instanceof Equipment))
+        this.reward = equipment;
+    }
+
+    unlock() {
+        return this.reward;
+    }
 }
