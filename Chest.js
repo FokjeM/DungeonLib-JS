15b691c94851981b836926d04864a810fc1e89fd
Riven Skaye <pretender0906@gmail.com>
2019-10-27 13:42:59 +0100

    Added more code, but progress is slow because JS classes are a nightmare. Gib Python! :pout:

--- Chest.js
+++ Chest.js
@@ -1,10 +1,16 @@
 class Chest {
     constructor(equipment) {
-        if(!(equipment instanceof Equipment))
+        if(!(equipment instanceof Equipment)){
+            throw new Exception("Not a type of equipment");
+        }
         this.reward = equipment;
+        this.unlocked = false;
     }
 
-    unlock() {
-        return this.reward;
+    empty() {
+        this.unlocked = true;
+        r = this.reward;
+        this.reward = null;
+        return r;
     }
 }
--- Room.js
+++ Room.js
@@ -1,3 +1,30 @@
+const NAMES = {
+    "weapon": ["Sword of HODOR", "Dagger of Shadows", "Fancy Toothpick"],
+    "armor": ["Chestplate of Butter", "Rack of Lamb", "Latex Catsuit"],
+    "helmet": ["Animal Skull", "Fine Mesh Sifter", "Itchy Beanie"],
+    "shield": ["Cardboard Square", "Feather Pillow", "Bouquet of Flowers"]
+}
+
 class Room {
-    
+    constructor(chest, contents=[], exit=false) {
+        this.chest = chest;
+        this.contents = contents;
+        this.hasExit = exit;
+    }
+
+    generateChest() {
+        randType = Math.floor(Math.random() * TYPES.length);
+        randATT = 0;
+        randDEF = 0;
+        if(TYPES[randType] == "weapon"){
+            randATT = Math.random() * 5;
+            randDEF = Math.random() * (Math.random() * -5);
+        }else{
+            randATT = Math.random() * (Math.random() * -5);
+            randDEF = Math.random() * 5;
+        }
+        // `eq` because it's short but not `e`, which is customarily for events
+        eq = new Equipment(NAMES[TYPES[randType]][Math.floor(Math.random() * TYPES.length)], TYPES[randType], randATT, randDEF);
+        this.chest = new Chest(eq);
+    }
 }
