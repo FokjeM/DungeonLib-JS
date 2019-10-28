187d9d209218d6bfec0fcff38a2928f390efdba1
Riven Skaye <pretender0906@gmail.com>
2019-10-28 15:49:57 +0100

    Fixed a vulnerability where one can easily keep generating new chests for a room

--- Room.js
+++ Room.js
@@ -12,23 +12,32 @@ class Room {
         this.hasExit = exit;
     }
 
+    get chest(){
+        return this.chest;
+    }
+
+    set chest(c){
+        this.chest = c;
+    }
+
     generateChest(equip) {
         if(equip){
-            this.chest = new Chest(equip);
-            return;
+            return new Chest(equip);
         }
         randType = Math.floor(Math.random() * TYPES.length);
         randATT = 0;
         randDEF = 0;
         if(TYPES[randType] == "weapon"){
             randATT = Math.random() * 5;
-            randDEF = Math.random() * (Math.random() * -5);
+            randDEF = Math.random() * (Math.random() * -5); // A weapon does not provide armor
         }else{
-            randATT = Math.random() * (Math.random() * -5);
+            randATT = Math.random() * (Math.random() * -5);// Armor does not provide attack power
             randDEF = Math.random() * 5;
         }
         // `eq` because it's short but not `e`, which is customarily for events
         eq = new Equipment(NAMES[TYPES[randType]][Math.floor(Math.random() * TYPES.length)], TYPES[randType], randATT, randDEF);
-        this.chest = new Chest(eq);
+        //Make sure this function cannot be called over and over again from the console, to prevent abuse for perfect armor and weapons.
+        delete this.generateChest;
+        return new Chest(eq);
     }
 }
